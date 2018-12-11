1e8278eedbce0891c6f07a60afa4f60c93136656
Yan-rlathotep <campo.yannis@gmail.com>
2018-12-11 17:39:29 +0100

    my commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { combinator } from 'postcss-selector-parser';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div class="grid">
+        <div class="menu-top">MENU</div>
+        <div class="menu-bot">ACTIONS</div>
+        <div class="main-display">DISPLAY</div>
+        <div class="nested">
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+          <div class="box">box</div>
+        </div>
       </div>
+
     );
   }
 }
